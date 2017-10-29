26125fc112ece8a34931dda863d9113c9b41a409
Ilya Balashov <ibalashov24@gmail.com>
2017-10-29 18:31:01 +0300

    Initial commit & first hometask

---
+++ homework_1/problem_2/problem2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    cout << "Введите целые a и b: ";
+    cin >> a >> b;
+
+    bool isNegative = false;
+    if (a < 0) {
+        a = -a;
+        isNegative = true;
+    }
+    if (b < 0) {
+        b = -b;
+    }
+
+    int result = 0;
+    while (a >= b)
+    {
+        a -= b;
+        result++;
+    }
+
+    cout << "Неполное частное от деления a на b: ";
+    cout << (isNegative ? result - b : result) << endl;
+
+    return 0;
+}
