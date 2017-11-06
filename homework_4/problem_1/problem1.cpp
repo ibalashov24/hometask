9ae16803a90e0ffc0696946c310d524ebafdd242
Ilya Balashov <ibalashov24@gmail.com>
2017-11-07 02:45:17 +0300

    Fixed issues found by Yurii Litvinon (1st problem)

--- homework_4/problem_1/problem1.cpp
+++ homework_4/problem_1/problem1.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
 #include <cmath>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-const int ADDITIONAL_CODE_BASE = 16;    // 16 binary digits
+const int ADDITIONAL_CODE_BASE = sizeof (int) * 8;
 
-string reverseCopy(string::const_iterator begin, string::const_iterator end)
+vector<bool> reverseCopy(vector<bool>::const_iterator begin,
+                         vector<bool>::const_iterator end)
 {
-    auto temp = string(begin, end);
+    auto temp = vector<bool>(begin, end);
     auto left = temp.begin();
     auto right = temp.end() - 1;
     while (left < right)
@@ -19,73 +21,59 @@ string reverseCopy(string::const_iterator begin, string::const_iterator end)
     return temp;
 }
 
-string reverseCopy(const std::string &outString)
+vector<bool> reverseCopy(const std::vector<bool> &outString)
 {
     return reverseCopy(outString.begin(), outString.end());
 }
 
-int convertToAdditionalCode(int binaryNumber)
+vector<bool> decToBin(int number)
 {
-    return ((1 << ADDITIONAL_CODE_BASE) + binaryNumber);
-}
+    vector<bool> result(ADDITIONAL_CODE_BASE);
 
-string decToBin(int number)
-{
-    string result = "";
-
-    if (number < 0)
+    for (int i = 0; i < ADDITIONAL_CODE_BASE; ++i)
     {
-        number = convertToAdditionalCode(number);
-    }
-
-    while (number > 0)
-    {
-        result += (number % 2 == 0 ? '0' : '1');
-        number /= 2;
-    }
-
-    while (result.size() < ADDITIONAL_CODE_BASE)
-    {
-        result += '0';
+        result[i] = (number >> i) & 1;
     }
 
     return result;
 }
 
-int binToDec(const string &binaryNumber)
+int binToDec(const vector<bool> &binaryNumber)
 {
     int result = 0;
-    int powerOfTwo = 1;
-    for (auto e : binaryNumber)
+    for (int i = ADDITIONAL_CODE_BASE - 1; i >= 0; --i)
     {
-        result += powerOfTwo * (e - '0');
-        powerOfTwo *= 2;
-    }
-
-    // If result >= 2 ^ (ADDITIONAL_CODE_BASE - 1) => negative
-    if (result >= powerOfTwo / 2)
-    {
-        // 2 ^ ADDITIONAL_CODE_BASE == powerOfTwo due to size of the string
-        result = -(powerOfTwo - result);
+        result = (result << 1) | (binaryNumber[i]);
     }
 
     return result;
 }
 
-string sumBinaryNumbers(const string &a, const string &b)
+vector<bool> sumBinaryNumbers(const vector<bool> &a, const vector<bool> &b)
 {
-    string tempString(ADDITIONAL_CODE_BASE, '0');
+    vector<bool> tempNumber(ADDITIONAL_CODE_BASE, false);
 
-    short carry = 0;
+    bool carry = false;
     for (int i = 0; i < ADDITIONAL_CODE_BASE; i++)
     {
-        short currentDigit = (a[i] - '0') + (b[i] - '0') + carry;
+        short currentDigit = a[i] + b[i] + carry;
 
-        tempString[i] = currentDigit % 2 + '0';
+        tempNumber[i] = currentDigit % 2;
         carry = currentDigit / 2;
     }
 
-    return tempString;
+    return tempNumber;
+}
+
+string toString(const vector<bool> &input)
+{
+    string result = "";
+    for (auto e : input)
+    {
+        result.push_back(e + '0');
+    }
+
+    return result;
 }
 
 int main()
@@ -101,8 +89,8 @@ int main()
     auto secondNumberBin = decToBin(secondNumber);
 
     cout << "Двоичное представление этих чисел в дополнительном коде: " << endl;
-    cout << reverseCopy(decToBin(firstNumber)) << ' ';
-    cout << reverseCopy(decToBin(secondNumber)) << endl;
+    cout << toString(reverseCopy(decToBin(firstNumber))) << ' ';
+    cout << toString(decToBin(secondNumber)) << endl;
 
     bool isNeedReverse = false;
     if (firstNumber < 0 && secondNumber < 0)
@@ -113,19 +101,21 @@ int main()
         isNeedReverse = true;
     }
 
-    string sumBinary = "0";
+    vector<bool> sumBinary;
     sumBinary = sumBinaryNumbers(firstNumberBin, secondNumberBin);
 
     int sumDecimal = binToDec(sumBinary);
     if (isNeedReverse)
     {
         sumDecimal = -sumDecimal;
-        sumBinary = decToBin(convertToAdditionalCode(sumDecimal));
+        sumBinary = decToBin(sumDecimal);
     }
 
     cout << "Сумма в двоичном формате: ";
-    cout << reverseCopy(sumBinary.begin(), sumBinary.end()) << endl;
+    cout << toString(reverseCopy(sumBinary.begin(), sumBinary.end())) << endl;
 
     cout << "Сумма в десятичном формате: ";
     cout << sumDecimal << endl;
+
+    return 0;
 }
