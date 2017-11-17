8efb7bc5be30dde9f5d2c6bc7b34d35c119bd1f0
Ilya Balashov <ibalashov24@gmail.com>
2017-11-17 21:19:44 +0300

    stack is ready

---
+++ homework_6/stack.cpp
@@ -0,0 +1,53 @@
+#include "stack.h"
+
+#include <iostream>
+
+struct stackStuff::Stack
+{
+    int *storage;
+
+    int size = 0;
+    int capacity = 0;
+};
+
+stackStuff::Stack *stackStuff::createStack(int maxSize = DEFAULT_SIZE)
+{
+    Stack *newStack = new Stack;
+    newStack->capacity = maxSize;
+    newStack->storage = new int[maxSize];
+
+    return newStack;
+}
+
+void stackStuff::deleteStack(Stack *toDeleteStack)
+{
+    delete[] toDeleteStack->storage;
+    delete toDeleteStack;
+}
+
+bool stackStuff::isEmpty(const Stack *inputStack)
+{
+    return (inputStack->size == 0);
+}
+
+void stackStuff::push(Stack *inputStack, int value)
+{
+    if (inputStack->size == inputStack->capacity)
+    {
+        std::cerr << "The stack is full!!!" << std::endl;
+        return;
+    }
+
+    inputStack->storage[inputStack->size++] = value;
+}
+
+int stackStuff::pop(Stack *inputStack)
+{
+    if (isEmpty(inputStack))
+    {
+        std::cerr << "The stack is empty!!!" << std::endl;
+        return -1;
+    }
+
+    return inputStack->storage[--inputStack->size];
+}
