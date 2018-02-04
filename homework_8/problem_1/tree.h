dd31bd0ead8f1a1a430830ca71b210f9ee4562d9
Ilya Balashov <ibalashov24@gmail.com>
2018-02-04 22:49:12 +0300

    Added comments

--- homework_8/problem_1/tree.h
+++ homework_8/problem_1/tree.h
@@ -6,7 +6,10 @@ namespace treeStuff
 {
     struct SplayTree;
 
+    // Constructs new empty splay tree
     SplayTree *createSplayTree();
+
+    // Fully deletes splay tree
     void deleteSplayTree(SplayTree *tree);
 
     // Inserts element (key; value) into the tree
@@ -14,10 +17,16 @@ namespace treeStuff
     void insert(SplayTree *tree,
                 const std::string &key,
                 const std::string &value);
+
+    // Check if element with given key is already in tree
     bool isInTree(SplayTree *tree,
                   const std::string &key);
+
+    // Returns value which corresponds to key (or empty string if not exists)
     std::string getValue(SplayTree *tree,
                          const std::string &key);
+
+    // Erases element with given key
     void deleteElement(SplayTree *tree,
                        const std::string &key);
 }
