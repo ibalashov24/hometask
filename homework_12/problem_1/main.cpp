f971314b8e1c1dd2e8951df078dcbed1218a9311
Ilya Balashov <ibalashov24@gmail.com>
2018-02-06 07:34:06 +0300

    Implemented problem 1

--- homework_12/problem_1/main.cpp
+++ homework_12/problem_1/main.cpp
@@ -5,93 +5,160 @@
 #include <algorithm>
 #include <vector>
 #include <array>
+#include <limits>
 
 #include "search.h"
 
 using namespace std;
 
-bool readSymbols(ifstream &input, list<char> buffer, int count)
-{
-    int readCount = 0;
-    char temp = '\0';
-    while (readCount < count && input >> temp)
-    {
-        buffer.push_back(temp);
-        ++readCount;
-    }
+using StopSymbolTable = array<int, numeric_limits<char>::max() + 1>;
+using SuffixTable = vector<int>;
+using PrefixTable = vector<int>;
 
-    return (readCount >= count);
-}
-
-
-// Returns first (from the end) position where example do not equal
-int compare(const list<char> &buffer, const std::string &example)
+// Returns first (from the end) position of string where example do not equal
+int findDiscrepancy(const string &searchString,
+					const std::string &example,
+					int beginPosition)
 {
     int examplePos = example.size() - 1;
-    auto bufferPos = buffer.rbegin();
+    int searchStringPos = beginPosition + example.size() - 1;
 
     while (examplePos >= 0 &&
-           bufferPos != buffer.rend() &&
-           examplePos[examplePos] == *bufferPos)
+           searchStringPos >= beginPosition &&
+           example[examplePos] == searchString[searchStringPos])
     {
         --examplePos;
-        ++bufferPos;
+        --searchStringPos;
     }
 
     return examplePos;
 }
 
-// Makes precalc for Boyer–Moore string search algorithm
-pair<vector<int>, array<int, sizeof(char)> >
-    make_precalc(const string &example)
+// Stop-symbol heuristic
+StopSymbolTable calcStopSymbols(const string &example)
 {
-    // Stop-symbol heuristic
-    array<int, sizeof(char)> stopSymbol;
-    fill(stopSymbol.begin(), stopSymbol.end(), example.size());
-    for (int i = example.size() - 2; i >= 0; --i)
+    StopSymbolTable stopSymbol;
+    fill(stopSymbol.begin(), stopSymbol.end(), -1);
+
+    for (size_t i = 0; i < example.size() - 1; ++i)
     {
-        if (stopSymbol[example[i]] == example.size())
-        {
-            stopSymbol[example[i]] -= i - 1;
-        }
+    	stopSymbol[example[i]] = i;
     }
 
-    // Suffix heuristic
-
-    ...
+    return stopSymbol;
 }
 
+PrefixTable reversedPrefixFunciton(string example,
+								   bool needReverse = false)
+{
+	PrefixTable result(example.size(), 0);
+
+	if (needReverse)
+	{
+		reverse(example.begin(), example.end());
+	}
+
+	for (int i = example.size() - 2; i >= 0; --i)
+	{
+		unsigned int j = example.size() - result[i + 1];
+		while (j != example.size() &&
+			   example[j - 1] != example[i])
+		{
+			j = example.size() - result[j];
+		}
+
+		result[i] = example.size() - j;
+		if (example[j - 1] == example[i])
+		{
+			result[i]++;
+		}
+	}
+
+	if (needReverse)
+	{
+		reverse(result.begin(), result.end());
+	}
+
+	return result;
+}
+
+// Suffix heuristic (suffixTable[i] == minimal shift for suffix with length i)
+SuffixTable calcSuffixes(const string &example)
+{
+	auto reversePrefixTable = reversedPrefixFunciton(example);
+	auto prefixTable = reversedPrefixFunciton(example, true);
+
+	SuffixTable result(example.size() + 1, example.size());
+	for (size_t i = 0; i < reversePrefixTable.size(); ++i)
+	{
+		if (i == 0 || reversePrefixTable[i] == 0 ||
+			reversePrefixTable[i - 1] <= reversePrefixTable[i])
+		{
+			const auto suffixPosition = example.size() - reversePrefixTable[i];
+			result[reversePrefixTable[i]] = suffixPosition - i;
+		}
+	}
+
+	for (size_t i = 1; i < result.size(); ++i)
+	{
+		if (static_cast<size_t>(result[i]) == example.size())
+		{
+			result[i] = example.size() - prefixTable.back();
+		}
+	}
+
+	return result;
+}
+
+// Returns position of the first entry in string or -1 if not found
+int findInString(const string &searchString, const string &example)
+{
+	auto stopSymbols = calcStopSymbols(example);
+	auto suffixShifts = calcSuffixes(example);
+
+	size_t currentPosition = 0;
+	while (currentPosition + example.size() <= searchString.size())
+	{
+		const int firstDiscrepancy = findDiscrepancy(searchString,
+													 example,
+													 currentPosition);
+		const char discrepancySymbol = searchString[firstDiscrepancy + currentPosition];
+
+		if (firstDiscrepancy < 0)
+		{
+			return currentPosition;
+		}
+
+
+		int stopSymbolMove = firstDiscrepancy - stopSymbols[discrepancySymbol];
+		int suffixMove = suffixShifts[example.size() - firstDiscrepancy - 1];
+
+		currentPosition += max(stopSymbolMove, suffixMove);
+	}
+
+	return -1;
+}
+
+// Returns position of the first entry in stream or -1 if not found
 int findFirstEntry(ifstream &input, const string &example)
 {
-    auto precalc = makePrecalc(example);
+	// Position of the first character of current line in the text
+	int positionOfFirst = 0;
 
-    // Buffer
-    list<char> lastTextSymbols;
+	string currentLine;
+	while (getline(input, currentLine))
+	{
+		const int firstEncounter = findInString(currentLine, example);
 
-    // Last read symbol position in the text
-    int currentTailPosition = 0;
-    int newTailPosition = example.size() - 1;
-    // Replenishes buffer
-    while (readSymbols(input, lastTextSymbols, newTailPosition - currentTailPosition))
-    {
-        lastTextSymbols.erase(lastTextSymbols.begin(),
-                              lastTextSymbols.begin() + newTailPosition - currentTailPosition));
-        currentTailPosition = newTailPosition;
+		if (firstEncounter != -1)
+		{
+			return firstEncounter + positionOfFirst;
+		}
 
-        int failPosition = compare(lastTextSymbols, example);
+		positionOfFirst += currentLine.size();
+	}
 
-        if (failPosition < 0)
-        {
-            break;
-        }
-
-        newTailPosition += max(precalc.first[failPosition + 1],     // Suffix
-                               precalc.second[lastTextSymbols.back()]); // Stop
-    }
-
-    return (newTailPosition != currentTailPosition ?
-                                return -1 :
-                                return currentTailPosition - example.size() + 1);
+	return -1;
 }
 
 int main()
@@ -101,6 +168,12 @@ int main()
     cin >> example;
 
     ifstream inputFile("input.txt");
+    if (!inputFile.is_open())
+    {
+    	cerr << "Unknown file!!!" << endl;
+    	return 0;
+    }
+
     int firstEntry = findFirstEntry(inputFile, example);
     inputFile.close();
 
