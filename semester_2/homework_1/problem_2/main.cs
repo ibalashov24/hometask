176fac5b194185ce7d532e647bc01c12f96c1f7e
Ilya Balashov <ibalashov24@gmail.com>
2018-02-14 18:14:25 +0300

    Implement problem 1-3

---
+++ semester_2/homework_1/problem_2/main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class FibonacciCalculator
+    {
+        private static int GetFibonacciNumber(int position)
+        {
+            int previousNumber = 1;
+            int currentNumber = 1;
+            
+            for (int i = 2; i < position; ++i)
+            {
+                currentNumber += previousNumber;
+                previousNumber = currentNumber - previousNumber;
+            }
+
+            return currentNumber;
+        }
+        public static void Main()
+        {
+            Console.Write("Enter Fibonacci number position (>= 1): ");
+            var position = System.Int32.Parse(Console.ReadLine());
+
+            var result = GetFibonacciNumber(position);
+
+            Console.WriteLine("Fibonacci[{0}] == {1}", position, result);
+        }
+    }
+}
