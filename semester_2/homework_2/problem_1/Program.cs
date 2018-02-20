4db5a9113809c3ec255f34b5ac227b2a6e4c7f00
Ilya Balashov <ibalashov24@gmail.com>
2018-02-21 00:44:01 +0300

    Implement problem 1 (HT #1)

---
+++ semester_2/homework_2/problem_1/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace Program
+{
+    using System;
+
+    internal class Program
+    {
+        private static readonly Command[] Commands = new Command[]
+        {
+            new Command(CommandNumbers.Help, "print this help"),
+            new Command(CommandNumbers.Push, "push element to the stack"),
+            new Command(CommandNumbers.Pop, "get (and delete) top element from the set"),
+            new Command(CommandNumbers.Exit, "exit the program")
+        };
+
+        private enum CommandNumbers
+        {
+            Exit,
+            Help,
+            Push,
+            Pop
+        }
+
+        public static void Main(string[] args)
+        {
+            PrintHelp();
+
+            var stack = new StackStuff.Stack<int>();
+
+            while (true)
+            {
+                Console.Write("Enter command: ");
+                var enteredCommand = int.Parse(Console.ReadLine());
+
+                if (CommandNumbers.IsDefined(typeof(CommandNumbers), enteredCommand))
+                {
+                    switch ((CommandNumbers)enteredCommand)
+                    {
+                        case CommandNumbers.Help:
+                            PrintHelp();
+                            break;
+                        case CommandNumbers.Push:
+                            Console.Write("Enter value: ");
+                            stack.Push(int.Parse(Console.ReadLine()));
+                            break;
+                        case CommandNumbers.Pop:
+                            if (stack.IsEmpty())
+                            {
+                                Console.WriteLine("The stack is empty!!!");
+                            }
+                            else
+                            {
+                                var topValue = stack.Pop();
+                                Console.WriteLine($"Top value is: {topValue}");
+                            }
+
+                            break;
+                        default:
+                            return;
+                    }
+
+                    Console.WriteLine("Success!");
+                }
+                else
+                {
+                    PrintHelp();
+                }
+            }
+        }
+
+        private static void PrintHelp()
+        {
+            foreach (var command in Commands)
+            {
+                Console.WriteLine($"Print {(int)command.Number} to {command.HelpMessage}");
+            }
+        }
+
+        private struct Command
+        {
+            public CommandNumbers Number;
+            public string HelpMessage;
+
+            public Command(CommandNumbers number, string message)
+            {
+                this.Number = number;
+                this.HelpMessage = message;
+            }
+        }
+    }
+}
---
+++ semester_2/homework_2/problem_1/Stack.cs
@@ -0,0 +1,53 @@
+﻿namespace StackStuff
+{
+    public class Stack<T> : IStack<T>
+    {
+        private int size = 0;
+        private StackElement top = default(StackElement);
+
+        public Stack()
+        {
+        }
+
+        public bool IsEmpty()
+        {
+            return this.size == 0;
+        }
+
+        public T Pop()
+        {
+            if (this.IsEmpty())
+            {
+                // TODO: Replace with exception
+                return default(T);
+            }
+            else
+            {
+                var returnValue = this.top;
+
+                this.top = this.top.Next;
+                --this.size;
+
+                return returnValue.Value;
+            }
+        }
+
+        public void Push(T value)
+        {
+            this.top = new StackElement(value, this.top);
+            ++this.size;
+        }
+
+        private class StackElement
+        {
+            public T Value;
+            public StackElement Next;
+
+            public StackElement(T value, StackElement next)
+            {
+                this.Value = value;
+                this.Next = next;
+            }
+        }
+    }
+}
