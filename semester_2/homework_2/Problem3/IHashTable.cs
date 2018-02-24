5f51593ac3c2df2889bfd25a9e38c897083bd12b
Ilya Balashov <ibalashov24@gmail.com>
2018-02-25 01:57:54 +0300

    Fix universal type's names

--- semester_2/homework_2/Problem3/IHashTable.cs
+++ semester_2/homework_2/Problem3/IHashTable.cs
@@ -1,20 +1,20 @@
 ﻿namespace HashTableStuff
 {
-    internal interface IHashTable<KeyType, ValueType>
+    internal interface IHashTable<TKey, TValue>
     {
         // Allows to access hash table
-        ValueType this[KeyType key] { get; set; }
+        TValue this[TKey key] { get; set; }
 
         // Adds (or modify) element of the hash table
-        void Add(KeyType key, ValueType value);
+        void Add(TKey key, TValue value);
 
         // Get hash table's fill factor
         double GetFillFactor();
 
         // Check if element with given key is in table
-        bool IsInTable(KeyType key);
+        bool IsInTable(TKey key);
 
         // Erases element with given key (if exists)
-        void Erase(KeyType key);
+        void Erase(TKey key);
     }
 }
