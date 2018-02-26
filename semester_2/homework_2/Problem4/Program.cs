3dcfd62d81916f95ad915a72a0eebdb343ab0f08
Ilya Balashov <ibalashov24@gmail.com>
2018-02-26 13:51:26 +0300

    Add stack selection

--- semester_2/homework_2/Problem4/Program.cs
+++ semester_2/homework_2/Problem4/Program.cs
@@ -9,9 +9,25 @@
             Console.WriteLine("Enter numeric expression (with spaces!!!):");
             var expression = Console.ReadLine();
 
-            var calculator = new StackCalculatorStuff.StackCalculator(
-                new StackStuff.Stack<double>(), expression);
+            StackCalculatorStuff.StackCalculator calculator;
 
+            Console.WriteLine("Enter 0 to use array-based stack " +
+                "or any another string to use reference-based stack");
+            var chosenStack = Console.ReadLine();
+    
+            if (chosenStack == "0")
+            {
+                Console.WriteLine("Using array-based stack");
+                calculator = new StackCalculatorStuff.StackCalculator
+                    (new StackStuff.ArrayStack<double>(), expression);
+            }
+            else
+            {
+                Console.WriteLine("Using reference-based stack");
+                calculator = new StackCalculatorStuff.StackCalculator
+                    (new StackStuff.Stack<double>(), expression);
+            }
+            
             var result = calculator.CalculateResult();
 
             Console.WriteLine($"Result == {result}");
