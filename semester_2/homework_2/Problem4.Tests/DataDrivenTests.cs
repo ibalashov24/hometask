75d9ec0abf6bf763cb23d49fb94652e25cd087f8
Ilya Balashov <ibalashov24@gmail.com>
2018-03-01 21:48:54 +0300

    Add unit tests for #4

---
+++ semester_2/homework_2/Problem4.Tests/DataDrivenTests.cs
@@ -0,0 +1,84 @@
+﻿namespace Problem4.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DataDrivenTests
+    {
+        private StackCalculatorStuff.StackCalculator stackCalculator;
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get => this.testContextInstance;
+            set => this.testContextInstance = value;
+        }
+
+        [TestInitialize]
+        public void CalculatorInitialization()
+        {
+            // Stacks are tested separately, so there is no difference which stack
+            this.stackCalculator = new StackCalculatorStuff.StackCalculator(
+                new StackStuff.Stack<double>(), "");
+        }
+
+        // Fully correct arguments
+        [TestMethod]
+        [DeploymentItem("CorrectTests.csv")]
+        [DataSource(
+        "Microsoft.VisualStudio.TestTools.DataSource.CSV",
+        "|DataDirectory|\\CorrectTests.csv",
+        "CorrectTests#csv",
+            DataAccessMethod.Sequential)]
+        public void DataOnWhichShouldBeCorrectResult()
+        {
+            // Program can not find column "Test" by name because of garbage
+            // in memory at the place of the column name iside DataRow (see debugger watches)
+            // I do not know how to fix this
+            var expression = this.testContextInstance.DataRow[0].GetType() != typeof(System.DBNull) ?
+                (string)this.testContextInstance.DataRow[0] : "";
+            var correctResult = double.Parse(
+                (string)this.testContextInstance.DataRow["Result"]);
+            
+            this.stackCalculator.Expression = expression;
+
+            Assert.AreEqual(
+                correctResult,
+                this.stackCalculator.CalculateResult(),
+                "Incorrect result!!!");
+        }
+
+        // Checks that calculator detects invalid arguments
+        [TestMethod]
+        [DeploymentItem("IncorrectTests.csv")]
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\IncorrectTests.csv",
+            "IncorrectTests#csv",
+             DataAccessMethod.Sequential)]
+        public void DataOnWhichShouldBeArgumentException()
+        {
+            // Program can not find column "Test" by name because of garbage
+            // in memory at the place of the column name iside DataRow (see debugger watches)
+            // I do not know how to fix this
+            var expression = this.testContextInstance.DataRow[0].GetType() != typeof(System.DBNull) ?
+                (string)this.testContextInstance.DataRow[0] : "";
+
+            this.stackCalculator.Expression = expression;
+
+            try
+            {
+                this.stackCalculator.CalculateResult();
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(ArgumentException));
+                return;
+            }
+
+            Assert.Fail("An exception was not thrown on the wrong test");
+        }
+    }
+}
---
+++ semester_2/homework_2/Problem4.Tests/StackTests.cs
@@ -0,0 +1,113 @@
+﻿namespace Problem4.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class StackTests
+    {
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void CheckSizeOfEmptyStack(bool useArrayStack)
+        {
+            var stack = SelectStack<object>(useArrayStack);
+            Assert.AreEqual(stack.IsEmpty(), true);
+        }
+
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void CheckSizeOfNonEmptyStack(bool useArrayStack)
+        {
+            var stack = SelectStack<object>(useArrayStack);
+            stack.Push(new object());
+            Assert.IsFalse(stack.IsEmpty());
+        }
+
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void PushNullMustWorkCorrectly(bool useArrayStack)
+        {
+            var stack = SelectStack<object>(useArrayStack);
+            stack.Push(null);
+            Assert.IsNull(stack.Pop());
+        }
+
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void StackMustBeEmptyAfterPushAndPop(bool useArrayStack)
+        {
+            var stack = SelectStack<object>(useArrayStack);
+            stack.Push(new object());
+            stack.Pop();
+            Assert.IsTrue(stack.IsEmpty());
+        }
+
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void SinglePushAndPopMustSaveValue(bool useArrayStack)
+        {
+            const string testString = "testtesttest";
+            var stack = SelectStack<string>(useArrayStack);
+
+            stack.Push(testString);
+            Assert.AreEqual(stack.Pop(), testString);
+        }
+
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void PopFromEmptyStackMustThrowAnException(bool useArrayStack)
+        {
+            var stack = SelectStack<int>(useArrayStack);
+
+            try
+            {
+                stack.Pop();
+            }
+            catch (Exception)
+            {
+                return;
+            }
+
+            Assert.Fail();
+        }
+
+        [DataTestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void MultiplePushAndPops(bool useArrayStack)
+        {
+            var stack = SelectStack<int>(useArrayStack);
+
+            for (int i = -150; i <= 150; ++i)
+            {
+                stack.Push(i);
+            }
+
+            for (int i = 150; i >= -150; --i)
+            {
+                Assert.AreEqual(i, stack.Pop());
+            }
+        }
+
+        private static StackStuff.IStack<T> SelectStack<T>(bool useArrayStack)
+        {
+            StackStuff.IStack<T> stack;
+            if (useArrayStack)
+            {
+                stack = new StackStuff.ArrayStack<T>();
+            }
+            else
+            {
+                stack = new StackStuff.Stack<T>();
+            }
+
+            return stack;
+        }
+    }
+}
