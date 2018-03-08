cbf14195de3bf267f0833ece2b8cdba850c5118b
Ilya Balashov <ibalashov24@gmail.com>
2018-03-08 14:51:11 +0300

    Add tests

---
+++ semester_2/homework_4/Problem1.Tests/DataDrivenTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problem1.Tests
+{
+    [TestClass]
+    public class DataDrivenTests
+    {
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        /// Tests which should pass
+        /// </summary>
+        [DataTestMethod]
+        [DeploymentItem("CorrectTests.csv")]
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\CorrectTests.csv",
+            "CorrectTests#csv",
+            DataAccessMethod.Sequential)]
+        public void CorrectTests()
+        {
+            var tree = new Tree.ExpressionTree((string)TestContext.DataRow[0]);
+
+            Assert.AreEqual(
+                double.Parse((string)TestContext.DataRow["Result"]), 
+                tree.Value);
+
+            Assert.AreEqual(
+                (string)TestContext.DataRow[0],
+                tree.ToString());
+
+            Assert.AreEqual(
+                (string)TestContext.DataRow["Infix"],
+                tree.GetInfixRepresentation());
+        }
+
+        [DataTestMethod]
+        [DeploymentItem("ExceptionTests.csv")]
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\ExceptionTests.csv",
+            "ExceptionTests#csv",
+            DataAccessMethod.Sequential)]
+        [ExpectedException(typeof(Tree.Exception.InvalidExpressionException))]
+        public void ExceptionTests()
+        {
+            var tree = new Tree.ExpressionTree((string)TestContext.DataRow[0]);
+
+            // In order to run calculation
+            var value = tree.Value;
+        }
+    }
+}
---
+++ semester_2/homework_4/Problem1.Tests/UnitTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problem1.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DividingByZeroShouldBeCathed()
+        {
+            var tree = new Tree.ExpressionTree("(/ 1 0)");
+            var treeValue = tree.Value;
+        }
+
+    }
+}
