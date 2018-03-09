4e8b08d658b72ef6910d1165360af86ffcc74153
Ilya Balashov <ibalashov24@gmail.com>
2018-03-09 17:16:22 +0300

    Add tests

---
+++ semester_2/homework_4/HW4-Problem2/Problem2.Tests/Problem2.Tests/UniqueListTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problem2.Tests
+{
+    [TestClass]
+    class UniqueListTests
+    {
+        private ListStuff.UniqueList<string> list;
+
+        [TestInitialize]
+        public void InitUniqueList()
+        {
+            var list = new ListStuff.UniqueList<string>();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ListStuff.Exception.AlreadyExistException))]
+        public void AddingNotUniqueElementShouldCauseException()
+        {
+            list.Insert("alalalaspbu", 0);
+            list.Insert("12345", 1);
+            list.Insert("abcdef", 0);
+            list.Insert("", 2);
+
+            list.Insert("abcdef", 2);
+        }
+    }
+}
