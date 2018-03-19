34c0461cba7120db17a8b66e4f288e21ead9dedc
Ilya Balashov <ibalashov24@gmail.com>
2018-03-19 17:41:58 +0300

    Add tests for GameMap

---
+++ semester_2/homework_5/Problem2/Problem2/Problem2.Tests/DifferentMapTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problem2.Tests
+{
+    [TestClass]
+    public class DifferentMapTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ semester_2/homework_5/Problem2/Problem2/Problem2.Tests/MapTests.cs
@@ -0,0 +1,97 @@
+﻿namespace Problem2.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.IO;
+    using System.Threading;
+
+    [TestClass]
+    public class MapTests
+    {
+        private static Game.Map.IMap testMap;
+        private static int mapHeight;
+        private static int mapWidth;
+
+        [ClassInitialize]
+        public static void InitClass(TestContext context)
+        {
+            var inputStream = new StreamReader("SimpleMap.in");
+
+            MapTests.mapWidth = int.Parse(inputStream.ReadLine());
+            MapTests.mapHeight = int.Parse(inputStream.ReadLine());
+
+            MapTests.testMap = new Game.Map.GameMap(inputStream, mapWidth, mapHeight);
+        }
+        
+        [TestMethod]
+        public void WidthAndHeightShouldBeReadCorrectly()
+        {
+            Assert.AreEqual(
+                MapTests.mapHeight,
+                MapTests.testMap.Height,
+                "Different heights!!!");
+            Assert.AreEqual(
+                MapTests.mapWidth,
+                MapTests.testMap.Width,
+                "Different width!!!");
+        }
+
+        [TestMethod]
+        public void MapClassShouldRememberBoardCellCorrectly()
+        {
+            var checkStream = new StreamReader("SimpleMapCheck.in");
+
+            for (int i = 0; i < MapTests.mapHeight; ++i)
+            {
+                var currentString = checkStream.ReadLine().TrimEnd(
+                    new char[] { ' ', '\n'});
+
+                var currentStringInMap = "";
+                for (int j = 0; j < MapTests.mapWidth; ++j)
+                {
+                    switch (MapTests.testMap.GetCell(i, j))
+                    {
+                        case Game.Map.CellType.Border:
+                            currentStringInMap += '#';
+                            break;
+                        case Game.Map.CellType.Free:
+                            currentStringInMap += '.';
+                            break;
+                        default:
+                            Assert.Fail();
+                            break;
+                    }
+                }
+
+                Assert.AreEqual(currentString, currentStringInMap);
+            }
+        }
+
+        [TestMethod]
+        public void MapShouldProceedNonExistentCellsCorrectly()
+        {
+            Assert.AreEqual(Game.Map.CellType.NotExist, MapTests.testMap.GetCell(-1, -1));
+            Assert.AreEqual(Game.Map.CellType.NotExist, MapTests.testMap.GetCell(-1, 0));
+            Assert.AreEqual(Game.Map.CellType.NotExist, MapTests.testMap.GetCell(0, -1));
+            Assert.AreEqual(Game.Map.CellType.NotExist, MapTests.testMap.GetCell(-10000, -10000));
+            Assert.AreEqual(Game.Map.CellType.NotExist, MapTests.testMap.GetCell(MapTests.mapHeight, MapTests.mapWidth));
+            Assert.AreEqual(Game.Map.CellType.NotExist, MapTests.testMap.GetCell(
+                100 * MapTests.mapHeight, 
+                10000 * MapTests.mapWidth));
+            Assert.AreEqual(Game.Map.CellType.NotExist, MapTests.testMap.GetCell(MapTests.mapHeight, MapTests.mapWidth - 1));
+            Assert.AreEqual(Game.Map.CellType.NotExist, MapTests.testMap.GetCell(MapTests.mapHeight - 1, MapTests.mapWidth));
+        }
+
+        [TestMethod]
+        public void MapShouldProceedEmptyMapCorrectly()
+        {
+            var stream = StreamReader.Null;
+            var testEmptyMap = new Game.Map.GameMap(stream, 0, 0);
+
+            Assert.AreEqual(0, testEmptyMap.Width);
+            Assert.AreEqual(0, testEmptyMap.Height);
+            Assert.AreEqual(
+                Game.Map.CellType.NotExist, testEmptyMap.GetCell(0, 0));
+        }
+    }
+}
