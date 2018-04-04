fea7dd8de4f1e2fa9e424e72c5f23a77ef34e76f
Ilya Balashov <ibalashov24@gmail.com>
2018-04-05 01:23:03 +0300

    Improve tests

--- semester_2/homework_4/HW4-Problem2/Problem2.Tests/Problem2.Tests/UniqueListTests.cs
+++ semester_2/homework_4/HW4-Problem2/Problem2.Tests/Problem2.Tests/UniqueListTests.cs
@@ -4,26 +4,56 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Problem2.Tests
 {
     [TestClass]
-    class UniqueListTests
+    public class UniqueListTests
     {
         private ListStuff.UniqueList<string> list;
 
         [TestInitialize]
         public void InitUniqueList()
         {
-            var list = new ListStuff.UniqueList<string>();
+            this.list = new ListStuff.UniqueList<string>();
+        }
+
+        [TestMethod]
+        public void SimpleTest()
+        {
+            this.list.Insert("1", 0);
+            this.list.Erase(0);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ListStuff.Exception.AlreadyExistException))]
         public void AddingNotUniqueElementShouldCauseException()
         {
-            list.Insert("alalalaspbu", 0);
-            list.Insert("12345", 1);
-            list.Insert("abcdef", 0);
-            list.Insert("", 2);
+            this.list.Insert("spbu", 0);
+            this.list.Insert("12345", 1);
+            this.list.Insert("abcdef", 0);
+            this.list.Insert("", 2);
 
-            list.Insert("abcdef", 2);
+            this.list.Insert("abcdef", 2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ListStuff.Exception.ElementDoesNotExistException))]
+        public void ErasingOfNonExistingElementShouldCauseException()
+        {
+            this.list.Insert("spbu", 0);
+
+            this.list.Erase(5);
+        }
+
+        [TestMethod]
+        public void AddingAndErasingManyUniqueElementsShouldWork()
+        {
+            for (int i = 0; i < 1000; ++i)
+            {
+                this.list.Insert((i * 3456 + 234).ToString(), i);
+            }
+
+            for (int i = 0; i < 1000; ++i)
+            {
+                this.list.Erase(0);
+            }
         }
     }
 }
