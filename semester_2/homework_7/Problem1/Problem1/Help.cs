a9b29bf4d062c85250418f85b679335f5f1b63ea
Ilya Balashov <ibalashov24@gmail.com>
2018-04-05 20:17:40 +0300

    Implemented problem 1

---
+++ semester_2/homework_7/Problem1/Problem1/Help.cs
@@ -0,0 +1,51 @@
+﻿// Fairly borrowed this file from HT #2
+
+namespace Problem1
+{
+    /// <summary>
+    /// Contains some user help stuff
+    /// </summary>
+    internal class Help
+    {
+        /// <summary>
+        /// Descriptions for the help commands
+        /// </summary>
+        public static readonly HelpMessage[] Instanses =
+        {
+            new HelpMessage(HelpCommands.ExitProgram, "exit program"),
+            new HelpMessage(HelpCommands.InsertElement, "insert element to the list"),
+            new HelpMessage(HelpCommands.EraseElement, "erase element"),
+            new HelpMessage(HelpCommands.CleanList, "clean list"),
+            new HelpMessage(HelpCommands.Size, "print list size"),
+            new HelpMessage(HelpCommands.PrintList, "print list content")
+        };
+
+        /// <summary>
+        /// Available help commands
+        /// </summary>
+        internal enum HelpCommands
+        {
+            ExitProgram,
+            InsertElement,
+            EraseElement,
+            CleanList,
+            Size,
+            PrintList
+        }
+
+        /// <summary>
+        /// Represents single help message
+        /// </summary>
+        internal struct HelpMessage
+        {
+            public HelpCommands CommandNumber;
+            public string Message;
+
+            public HelpMessage(HelpCommands command, string message)
+            {
+                this.CommandNumber = command;
+                this.Message = message;
+            }
+        }
+    }
+}
---
+++ semester_2/homework_7/Problem1/Problem1/Program.cs
@@ -0,0 +1,98 @@
+﻿// Fairly borrowed this file from HT #2
+
+namespace Problem1
+{
+    using System;
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new ListStuff.List<int>();
+
+            PrintHelp();
+
+            while (true)
+            {
+                Console.Write("Enter command: ");
+                if (!int.TryParse(Console.ReadLine(), out var currentCommandNumber))
+                {
+                    Console.WriteLine("Command must be integer!");
+                    continue;
+                }
+
+                if (Enum.IsDefined(typeof(Help.HelpCommands), currentCommandNumber))
+                {
+                    switch ((Help.HelpCommands)currentCommandNumber)
+                    {
+                        case Help.HelpCommands.ExitProgram:
+                            Console.WriteLine("Good bye!");
+                            return;
+                        case Help.HelpCommands.InsertElement:
+                            try
+                            {
+                                Console.Write("Enter new element value: ");
+                                var insertValue = int.Parse(Console.ReadLine());
+                                Console.Write($"Enter position (0..{list.Count}): ");
+                                var position = int.Parse(Console.ReadLine());
+
+                                list.Insert(position, insertValue);
+                            }
+                            catch (System.FormatException)
+                            {
+                                Console.WriteLine("Incorrect format!!!");
+                            }
+
+                            break;
+                        case Help.HelpCommands.EraseElement:
+                            Console.Write($"Enter delete position (0 <= pos < {list.Count}): ");
+                            var deletePosition = int.Parse(Console.ReadLine());
+                            list.RemoveAt(deletePosition);
+                            break;
+                        case Help.HelpCommands.CleanList:
+                            list.Clear();
+                            Console.WriteLine("List is empty now");
+                            break;
+                        case Help.HelpCommands.Size:
+                            Console.WriteLine($"List size == {list.Count}");
+                            break;
+                        case Help.HelpCommands.PrintList:
+                            Console.WriteLine("List content: ");
+                            PrintList(list);
+                            break;
+                        default:
+                            PrintHelp();
+                            break;
+                    }
+                }
+                else
+                {
+                    PrintHelp();
+                }
+            }
+        }
+
+        private static void PrintHelp()
+        {
+            foreach (var command in Help.Instanses)
+            {
+                Console.WriteLine($"Print {(int)command.CommandNumber} to {command.Message}");
+            }
+        }
+
+        private static void PrintList(ListStuff.List<int> list)
+        {
+            if (list.IsEmpty())
+            {
+                Console.WriteLine("Empty");
+                return;
+            }
+
+            foreach (var listInstance in list)
+            {
+                Console.Write($"{listInstance} ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
