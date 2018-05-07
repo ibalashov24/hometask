7e6f57d45a8fab9f5b2693f0d55ab6c0f8e15a59
Ilya Balashov <ibalashov24@gmail.com>
2018-05-08 00:31:38 +0300

    Add Filter() function (modified from HT#5)

---
+++ semester_2/homework_9/Problem1/Problem1/ListUtilities.cs
@@ -0,0 +1,29 @@
+﻿namespace Utility
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Provides some utilities for List generic
+    /// </summary>
+    public static class ListUtilities
+    {
+        /// <summary>
+        /// Constructs new list from given list's elements which satisfy predicate
+        /// </summary>
+        /// <typeparam name="T">List element type</typeparam>
+        /// <param name="list">Input sequence</param>
+        /// <param name="predicate">Predicate to be checked</param>
+        /// <returns>Filtered list</returns>
+        public static IEnumerable<T> Filter<T>(IEnumerable<T> list, Predicate<T> predicate)
+        {
+            foreach (var element in list)
+            {
+                if (predicate(element))
+                {
+                    yield return element;
+                }
+            }
+        }
+    }
+}
