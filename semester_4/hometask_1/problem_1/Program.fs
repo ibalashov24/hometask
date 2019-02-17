f187a906061bbd34e2574435f8eb3a26b77751ad
Ilya Balashov <ibalashov24@gmail.com>
2019-02-17 18:18:28 +0300

    Implement first problem

---
+++ semester_4/hometask_1/problem_1/Program.fs
@@ -0,0 +1,7 @@
+﻿let userInput = System.Console.ReadLine() |> int
+let rec factorial x = if x <= 1 then 1 else x * factorial(x - 1)
+let output = 
+    if userInput < 0 then 
+        printfn "Number should be >= 0" 
+    else 
+        printfn "%d" (factorial userInput)    
