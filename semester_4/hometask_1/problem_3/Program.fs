a312c8c877c57f62f37cd0b5ba998ba97cc22ab0
Ilya Balashov <ibalashov24@gmail.com>
2019-02-17 22:36:02 +0300

    Fix bug with input in #3

--- semester_4/hometask_1/problem_3/Program.fs
+++ semester_4/hometask_1/problem_3/Program.fs
@@ -1,14 +1,4 @@
-﻿let rec readList est acc = 
-    if est <= 0 then
-        acc
-    else
-        readList (est - 1) (acc @ [System.Console.ReadLine()])
-
-let userInputList =
-    printf "Enter size: "
-    let listSize = System.Console.ReadLine() |> int
-    printf "Enter list (one item per line):\n"
-    readList listSize []
+﻿let inputList = [ 5; -1; 10; 16; 3 ]
 
 let reversedList list =
     let rec reverse list buf = 
@@ -17,5 +7,5 @@ let reversedList list =
             | head :: tail -> reverse tail (head :: buf)
     reverse list []
 
-printfn "Reversed: %A" (reversedList userInputList) 
+printfn "Reversed: %A" (reversedList inputList) 
 
