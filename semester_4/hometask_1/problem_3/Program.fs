4c75a82ebdaceab0754cea456922ec939191b36d
Ilya Balashov <ibalashov24@gmail.com>
2019-02-17 21:31:26 +0300

    Implement problem 3

---
+++ semester_4/hometask_1/problem_3/Program.fs
@@ -0,0 +1,21 @@
+﻿let rec readList est acc = 
+    if est <= 0 then
+        acc
+    else
+        readList (est - 1) (acc @ [System.Console.ReadLine()])
+
+let userInputList =
+    printf "Enter size: "
+    let listSize = System.Console.ReadLine() |> int
+    printf "Enter list (one item per line):\n"
+    readList listSize []
+
+let reversedList list =
+    let rec reverse list buf = 
+        match list with
+            | [] -> buf
+            | head :: tail -> reverse tail (head :: buf)
+    reverse list []
+
+printfn "Reversed: %A" (reversedList userInputList) 
+
