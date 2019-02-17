3f217716859043575f7c60e6cf20a0e59ab76ed5
Ilya Balashov <ibalashov24@gmail.com>
2019-02-17 18:34:51 +0300

    Add tail call

--- semester_4/hometask_1/problem_1/Program.fs
+++ semester_4/hometask_1/problem_1/Program.fs
@@ -1,7 +1,14 @@
 ﻿let userInput = System.Console.ReadLine() |> int
-let rec factorial x = if x <= 1 then 1 else x * factorial(x - 1)
+
+let rec factorialWithAccumulation x acc = 
+    if x <= 1 then 
+        acc 
+    else 
+        factorialWithAccumulation (x - 1) (acc * x)
+let factorial x = factorialWithAccumulation x 1
+
 let output = 
     if userInput < 0 then 
         printfn "Number should be >= 0" 
     else 
-        printfn "%d" (factorial userInput)    
+        printfn "%d" (factorial userInput)
