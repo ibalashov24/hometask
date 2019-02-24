31bf9995d7e4dee69926750d12e72fa575c69e14
Ilya Balashov <ibalashov24@gmail.com>
2019-02-24 22:46:34 +0300

    Implement problem #1

---
+++ semester_4/hometask_2/problem_1/Program.fs
@@ -0,0 +1,17 @@
+﻿namespace Problem1
+
+module Main =
+    let list = [5; 17; 8; 1; 2; 3; 8; 8; -100; 17] 
+    let sample = 0   
+
+    let findEntry list elem = 
+        let rec findEntryRecursive (list : int list) (elem : int) i =
+            match list with
+                | [] -> -1
+                | head :: tail -> 
+                    if head = elem then i else findEntryRecursive tail elem (i + 1)
+
+        findEntryRecursive list elem 0
+
+    printfn "First entry (-1 <-> not found): %d" <| findEntry list sample
+   
