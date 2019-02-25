61e409089ab1cb8a5e79421a5c8d9bd67d9fa941
Ilya Balashov <ibalashov24@gmail.com>
2019-02-25 18:51:08 +0300

    Prepare version of soultion for VS

---
+++ semester_4/hometask_2_VS/problem_1.Tests/Test.fs
@@ -0,0 +1,33 @@
+﻿namespace Problem1
+
+module Tests =
+    open NUnit.Framework
+    open FsUnit
+
+    [<Test>]
+    let ``Smoke test`` () =
+        let list = [1; 2; 3; -1; 23423; -4234; 0; 3]
+        let sample = -1
+
+        sample |> Main.findEntry list |> should equal 3
+
+    [<Test>]
+    let ``Duplicate item should be found normally`` () =
+        let list = [1; 2; 3; 23423; -4234; -1; -1; 0; 3; 1; -1]
+        let sample = -1
+
+        sample |> Main.findEntry list |> should equal 5
+
+    [<Test>]
+    let ``Non-existing element should not be found`` () =
+        let list = [1; 2; 3; 23423; -4234; -1; -1]
+        let sample = 42
+
+        sample |> Main.findEntry list |> should equal -1
+
+    [<Test>]
+    let ``Function should work correctly on empty list`` () =
+        let list = []
+        let sample = 42
+
+        sample |> Main.findEntry list |> should equal -1
