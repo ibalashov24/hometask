f7e3819f4ac0b45fa5b91152f660132cea064b2d
Ilya Balashov <ibalashov24@gmail.com>
2019-03-16 01:28:45 +0300

    Implement problem 4

---
+++ semester_4/hometask_3/Problem4/Program.fs
@@ -0,0 +1,20 @@
+﻿namespace Problem4
+
+module Main = 
+    /// Checks if given number is prime
+    let isPrime number =
+        let rec isPrimeRec number i = 
+            if i <> number && number % i = 0 || number < 2 then
+                false
+            else if i > number / 2 then 
+                true
+            else 
+                isPrimeRec number (i + 1)
+
+        isPrimeRec number 2
+
+    /// Generates infinite sequence of prime number
+    let generateInfinitePrimeSequence () =
+        id |> Seq.initInfinite |> Seq.filter isPrime
+
+    generateInfinitePrimeSequence () |> printfn "%A"
