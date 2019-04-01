d3eca04a978911a43e00fd4c98f5addf98965677
Ilya Balashov <ibalashov24@gmail.com>
2019-04-01 10:03:05 +0300

    Implement problem 1

---
+++ semester_4/hometask_5/Problem1/Program.fs
@@ -0,0 +1,25 @@
+﻿module Main
+
+open Stack
+
+// Checks bracket sequence for correctness
+let isBracketsCorrect (seq : string) =
+    let rec checker seq openBrackets =
+        match seq with
+            | [] when (openBrackets |> Stack.isEmpty) -> true
+            | '(' :: tail -> checker tail (openBrackets |> Stack.push '(')
+            | '{' :: tail -> checker tail (openBrackets |> Stack.push '{')
+            | '<' :: tail -> checker tail (openBrackets |> Stack.push '<')
+            | _ when (openBrackets |> Stack.isEmpty) -> false
+            | ')' :: tail when (openBrackets |> Stack.top) = '(' ->
+                    checker tail (openBrackets |> Stack.pop) 
+            | '}' :: tail when (openBrackets |> Stack.top) = '{' ->
+                    checker tail (openBrackets |> Stack.pop) 
+            | '>' :: tail when (openBrackets |> Stack.top) = '<' ->
+                    checker tail (openBrackets |> Stack.pop) 
+            | _ -> false
+
+    checker (seq |> Seq.toList) Stack.EmptyStack
+
+"{}" |> isBracketsCorrect |> printfn "%b"
+    
